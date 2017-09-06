814d113215aed6895460e868c0a909913a208fe3
Garrett Carson <garrett.carson@bdcsoft.com>
2017-09-06 13:44:45 -0500

    modularize

--- Reset.py
+++ Reset.py
@@ -1,14 +1,14 @@
 import bpy
 
 class reset():
+	@staticmethod
 	def reset():
 		#bpy.ops.wm.read_factory_settings()
-
+		
 		for scene in bpy.data.scenes:
 			for obj in scene.objects:
 				scene.objects.unlink(obj)
-
-		# only worry about data in the startup scene
+		
 		for bpy_data_iter in (
 			bpy.data.objects,
 			bpy.data.meshes,
