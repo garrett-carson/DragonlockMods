40dd27aa51018eddf01c6d09a9443053d9b4d17f
Garrett Carson <garrett.carson@bdcsoft.com>
2017-09-06 13:14:27 -0500

    modularize somewhat

--- Reset.py
+++ Reset.py
@@ -1,20 +1,19 @@
 import bpy
 
-def reset_blend():
-    bpy.ops.wm.read_factory_settings()
+class reset():
+	def reset():
+		#bpy.ops.wm.read_factory_settings()
 
-    for scene in bpy.data.scenes:
-        for obj in scene.objects:
-            scene.objects.unlink(obj)
+		for scene in bpy.data.scenes:
+			for obj in scene.objects:
+				scene.objects.unlink(obj)
 
-    # only worry about data in the startup scene
-    for bpy_data_iter in (
-            bpy.data.objects,
-            bpy.data.meshes,
-            bpy.data.lamps,
-            bpy.data.cameras,
-            ):
-        for id_data in bpy_data_iter:
-            bpy_data_iter.remove(id_data)
-
-reset_blend()
+		# only worry about data in the startup scene
+		for bpy_data_iter in (
+			bpy.data.objects,
+			bpy.data.meshes,
+			bpy.data.lamps,
+			bpy.data.cameras,
+			):
+			for id_data in bpy_data_iter:
+				bpy_data_iter.remove(id_data)
